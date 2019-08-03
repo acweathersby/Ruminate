a4855d1950ad7b86b15990e129039548ed2a1939
Anthony Weathersby <anthonycweathersby@gmail.com>
2019-08-02 18:03:51 -0600

    working on tests, readme

--- README.md
+++ README.md
@@ -1,33 +1,137 @@
-# GRAZE
+# GRAZE NOTES
 
 #### seriously, an experimental work-in-progress
 
-Simple Notes 📓
+For those quick, daydreaming ideas that quickly float off, and the rest. 📓
 
-# Creating and Interacting with notes
 
-For those quick, daydreaming ideas that quickly float off, and the rest. 
+version 0.*.* - The console miester
 
-- v 0.1.0
-- - Notes can be titled, tagged, and bagged (stored and retrieved 😉).  
-- - Simple query systems allows searching for specific notes. 
+#usage
+```bash
 
-v 0.2.0
-- - Linking of notes into other notes.
-- - Organizer for notes. 
+yarn install graze
 
-v 0.3.0 
-- - extension system
-- - exporting of notes into other doc types. 
-- - importing of notes from documents using special comment fields. 
+graze 
 
-# Note hosting options
+```
 
-- v 0.1.0
-- - JSON file based note server
+Methodes - 
+	Lookup Note
+	Add Note
+	Edit Note
+	Delete Note
 
-- v 0.2.0
-- - Self hosting DB driven note server that can be installed as a service
+Note structure
+	Note Body
+	Note Title
+	Note Tags
+	Note Ref Count
+	Note UID
+	Note Refs
 
-- v 0.4.0
-- - Integration with existing hosting services for note taking availability anywhere
+Adding Note
+	Use Query To Identify note 
+	Check to see if the host is refered in any of claim's references
+	If passed ref test insert note, update claim ref count. Add claim to the existing notes references.
+
+
+Note Structure. Notes maintain UID references references to the 
+
+Note : {	
+	
+}
+
+
+
+API 
+createUID
+
+Graze Server API:
+
+	storenoteData
+	retrieveUID
+	removeUID
+	query
+
+
+Graze Runtime
+
+	properties:
+
+		note_count : number of notes stored in the current store. 
+
+		cache : references to notes that have been retrieved from store. 
+
+		sort_indexes: 
+
+				create_time : 1
+				modify_time : 2
+				id : 3
+				tags : 4
+				body : 5
+
+
+	methods:
+
+		createUID - 
+
+			Returns a UID value
+
+		createNote -
+			args => note_id : string [, note_tags : array of strings || csv string[, body : string[, UID]]]
+
+			Creates a new note with a new UID, setting creation time stamp to moment of call. At this point, this note is NOT 
+			stored within a data store. Either the notes store/save method must be called, or the note must be passed to graze.store()
+			If UID is supplied, this must be a UID value unique to the store, otherwise, a new note will not be created and the 
+
+			return new Note.
+
+		retrieve - 
+			args => collection or note name : string | uid [,   query : string]
+
+			Retrieves a single note or a note collection. This function accepts a UID, the string name of the desired note, or a list of UID's, or the empty string.
+			An optional query string can be supplied to further reduce the results. This function may return all notes if not supplied with any arguments, so use with caution. 
+
+		remove -
+
+			arg => note : Note | Array[, +]
+
+			Removes a note from the store. Accepts an array of notes, or multiple arguments of notes. Removes the notes from the store. Notes are note fully removed unless the 
+			reference parameter is set to zero, ie, there are no references to that note. Other wise, only the note's removed flag is set and the note can no longer be retrieved from the store. 
+
+		store - 
+			args => note : Note | Array[, +]
+
+			Stores note(s) in the data store, or updates the stored copy with the new value;
+
+Note Container
+
+	Stores a collection of notes. Notes can be indexed by index location
+
+	properties:
+
+		length: number of notes in the container. 
+
+	methods:
+
+		sort:	Returns a new container with nodes sorted according to argument
+				argument can either be a graze.sort_index value or a function
+
+
+Note
+	properties:
+		
+		uid: 128bit string number.  
+		modify-date: UNIX TIME STAMP
+		creation-date: UNIX TIME STAMP
+		id: string - docs.test.me
+		tags: string - csv tag names. could be auto generated
+		body: string or line list or some appropriate data structure. 
+		refs: array of UIDS of referred notes
+
+	methods:
+
+	 	store/save 	: async - saves the note's data to the backing server. returns true if the save was successfull, or returns false. 
+	 	render 		: async - render the note's message data into string output, transforming reducing any note references. 
+ 					Accepts a transformer function argument that's used to customize the transformation process. 
--- test/test.js
+++ test/test.js
@@ -1,8 +1,173 @@
 import chai from "chai";
-import graze from "../source/graze.js";
+import graze_constructor from "../source/graze.js";
+import graze_json_server from "../source/json_server.js";
 
 chai.should();
 
-it("warning: empty test", function(){
-	graze.should.not.be.undefined;
+graze_json_server.connect("./json_test_db.json");
+
+define("Graze Utilites", function(){
+
+	it("UID", function(){
+		const size = 1000000;
+		//Stress test for UID.
+		var UID_LIST = new Array(1000000).fill(null).map(()=>graze.createUID());
+		var set = new Set();
+
+		for(const uid in UID_LIST)
+			if(!set.has(uid.string))
+				set.add(uid.string)
+			else
+				throw new Error("Duplicate String Found " + uid.string)
+	})
+
+
+	
 })
+
+
+define("Graze Testing - JSON BACKED", graze_test_suite(graze_constructor, graze_json_server, {
+	type:"JSON BACKED",
+	server_id:"JSONDB"
+}))
+
+function graze_test_suite(GrazeConstructor, server, params) {
+    return function() {
+    	const graze = new GrazeConstructor();
+
+        beforeEach(function() {
+        	graze.connect(server);
+        })
+
+        afterEach(function(){
+        	graze.disconnect();
+			//Clear all data from server. Server specific and very dangerous
+        	//This function needs to be called three times to actually work
+        	server.implode();
+        	server.implode();
+        	server.implode();
+        })
+
+        it("warning: empty test", function() {
+            graze.should.not.be.undefined;
+        })
+
+        it(`connect to server - ${params.type}`, function() {
+
+        	const graze = new GrazeConstructor();
+
+            graze.server.should.be.null;
+
+            graze.connect(server);
+
+            graze.server.type.should.equal(params.server_id)
+
+            graze.disconnect();
+
+            graze.server.should.be.null;
+        })
+
+        it("create UID", function() {
+
+            const uid = graze.createUID();
+
+            uid.length.should.equal(16);
+
+            const note = graze.createNote("Temp Name", "tagA, tagB, tagC", "Message");
+
+            note.should.have.uid;
+
+            note.uid.length.should.equal(16);
+
+            note.string.length.should.equal(16);
+
+            const note2 = graze.createNote("Temp Name", "tagA, tagB, tagC", "Message");
+
+            note2.uid.length.should.equal(16);
+
+            note2.uid.string.should.not.equal(note.uid.string);
+        })
+
+        it("store and retrieve - basic", async function(){
+
+			const noteA = graze.createNote("Temp Name A", "tagA, tagB, tagC", "Message A");
+			
+			noteA.save();
+			
+			const noteB = graze.createNote("Temp Name B", "tagA, tagB, tagC", "Message B");
+			
+			graze.store(noteB);
+
+			const noteAd = await graze.retrieve(noteA.uid);
+			const noteBd = await graze.retrieve(noteB.name);
+
+			noteA.message_string.should.equal(noteAd.message_string);
+			noteB.message_string.should.equal(noteBd.message_string);
+			noteA.message_string.should.not.equal(noteBd.message_string);
+
+			noteA.uid.string.should.equal(noteAd.uid.string);
+        })
+
+        it("store and retrieve - collection", async function(){
+
+        	const noteA = graze.createNote("temp.Temp Name A", "tagA, tagB, tagC", "Message A");
+			
+			noteA.save();
+			
+			const noteB = graze.createNote("temp.Temp Name B", "tagA, tagB, tagC", "Message B");
+			
+			graze.store(noteB);
+			
+			const noteC = graze.createNote("temp.temp.Temp Name B", "tagA, tagB, tagC", "Message B");
+			
+			const notes = await graze.retrieve("temp.");
+
+			notes.length.should.equal(2);
+			
+			notes.sort(graze.sort_indexes.create_time)[0].message_string.should.equal(noteA.message_string.name);
+			notes.sort(graze.sort_indexes.create_time)[1].message_string.should.equal(noteB.message_string.name);
+
+			const notes2 = await graze.retrieve("temp.temp.");
+
+			notes2.length.should.equal(1)
+			notes2[0].message_string.should.equal(noteC.message_string);
+        })
+
+        it("store and retrieve - search", async function(){
+
+        	const noteA = graze.createNote("temp.Temp Name A", "tagA, tagB, tagC", "Message A");
+			
+			noteA.save();
+			
+			const noteB = graze.createNote("temp.Temp Name B", "tagA, tagB, tagC", "Message B");
+			
+			graze.retrieve(noteB);
+			
+			const noteC = graze.createNote("temp.temp.Temp Name B", "tagA, tagB, tagC", "Message B");
+				
+			noteC.store();
+
+			const notes = await graze.retrieve("", "temp.temp. && Name B && Message B");
+
+			notes.length.should.equal(1);
+
+			notes[0].message_string.should.equal(noteC.message_string);
+        })
+
+        it("Renders note refernced inside another note", async function(){
+        	
+        	const noteA = graze.createNote("temp.Temp Name A", "tagA, tagB, tagC", "inside");
+			
+			noteA.save();
+			
+			const noteB = graze.createNote("temp.Temp Name B", "tagA, tagB, tagC", `referenced note text: ((${noteA.uid}))`);
+			
+			//note does not need to be saved in order to take advantage of reference rendering.
+
+			(await noteB.render()).should.equal("referenced note text: inside");
+        })
+
+        //storage
+        //search
+    }
+}
